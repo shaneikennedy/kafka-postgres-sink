8b71efcdf705193a14a9994ece255ae9ec65ebd9
Shane Kennedy <shane.kennedy19@gmail.com>
2024-05-21 14:45:50 +0200

    Deadletter queuing

--- src/main/groovy/org/example/Main.groovy
+++ src/main/groovy/org/example/Main.groovy
@@ -5,7 +5,6 @@ import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.Timestamp
 import java.time.Duration
-import java.util.concurrent.Future
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.consumer.ConsumerRecords
@@ -13,12 +12,10 @@ import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.clients.producer.ProducerRecord
-import org.apache.kafka.clients.producer.RecordMetadata
 import org.apache.kafka.common.header.Headers
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
 import org.slf4j.LoggerFactory
-
 
 static void main(String[] args) {
 	def log = LoggerFactory.getLogger(Main)
@@ -30,7 +27,7 @@ static void main(String[] args) {
 	}
 
 	def pg = new PostgresClient()
-	def handler = new KafkaHandler(new Consumer(), new QuarantineProducer())
+	def handler = new KafkaHandler(new Consumer(), new QuarantineProducer(), new DeadLetterProducer())
 	handler.start(List.of(config.topic.name, config.quarantine.name), { ConsumerRecord record -> pg.insertPayment(record.value() as String) })
 }
 
@@ -58,11 +55,32 @@ class Consumer {
 	}
 }
 
+class DeadLetterProducer {
+	KafkaProducer producer
+	def bootstrapServers = "127.0.0.1:9092"
+	def groupId = "my-fourth-application"
+
+	DeadLetterProducer() {
+		def props = new Properties()
+		props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+		props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.getName())
+		props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.getName())
+		props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId)
+		props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
+		producer = new KafkaProducer<>(props)
+	}
+
+	void sendSynchronously(String key, String value, Headers headers) {
+		ProducerRecord quarantineRecord = new ProducerRecord("demo_java_dlq", 0, key, value, headers)
+		producer.send(quarantineRecord)
+		producer.flush()
+	}
+}
+
 class QuarantineProducer {
 	KafkaProducer producer
 	def bootstrapServers = "127.0.0.1:9092"
 	def groupId = "my-fourth-application"
-	def logger = LoggerFactory.getLogger(QuarantineProducer)
 
 	QuarantineProducer() {
 		def props = new Properties()
@@ -85,11 +103,13 @@ class QuarantineProducer {
 class KafkaHandler {
 	Consumer consumer
 	QuarantineProducer quarantineProducer
+	DeadLetterProducer dlqProducer
 	def logger = LoggerFactory.getLogger(KafkaHandler)
 
-	KafkaHandler(Consumer consumer, QuarantineProducer quarantineProducer) {
+	KafkaHandler(Consumer consumer, QuarantineProducer quarantineProducer, DeadLetterProducer dlqProducer) {
 		this.consumer = consumer
 		this.quarantineProducer = quarantineProducer
+		this.dlqProducer = dlqProducer
 	}
 
 	void start(List<String> topics, Closure callback) {
@@ -106,6 +126,7 @@ class KafkaHandler {
 					logger.info("RETRY COUNT: $retryCount")
 					if (retryCount > 3) {
 						logger.error("Dropping message after 3 retries")
+						dlqProducer.sendSynchronously(it.key(), it.value(), it.headers())
 					} else {
 						quarantineProducer.sendSynchronously(it.key(), it.value(), it.headers())
 					}
