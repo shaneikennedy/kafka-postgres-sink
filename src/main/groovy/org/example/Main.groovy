c70b1e15e450f7641361c569e8cb932886b59ad6
Shane Kennedy <shane.kennedy19@gmail.com>
2024-05-22 17:13:11 +0200

    Dumb properties and a very dumb quarantine

--- src/main/groovy/org/example/Main.groovy
+++ src/main/groovy/org/example/Main.groovy
@@ -6,22 +6,36 @@ import java.sql.Connection
 import java.sql.DriverManager
 import java.sql.Timestamp
 import java.time.Duration
+import java.util.concurrent.Future
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.clients.consumer.ConsumerRecords
 import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
 import org.slf4j.LoggerFactory
 
 
 static void main(String[] args) {
+	def log = LoggerFactory.getLogger(Main)
+	def config
+	try {
+		config = new Config()
+	} catch (IOException e) {
+		log.error("Could not load config.properties", e)
+	}
+
 	def pg = new PostgresClient()
-	def consumer = new Consumer()
-	consumer.start("demo_java", { ConsumerRecord record -> pg.insertPayment(record.value() as String)})
+	def handler = new KafkaHandler(new Consumer(), new QuarantineProducer())
+	handler.start(config.topic.name, { ConsumerRecord record -> pg.insertPayment(record.value() as String) })
 }
 
 class Consumer {
 	KafkaConsumer consumer
-
 	def bootstrapServers = "127.0.0.1:9092"
 	def groupId = "my-fourth-application"
 
@@ -35,6 +49,47 @@ class Consumer {
 		consumer = new KafkaConsumer<>(props)
 	}
 
+	void subscribe(List<String> topics) {
+		consumer.subscribe(topics)
+	}
+
+	ConsumerRecords poll(Duration dur) {
+		return consumer.poll(dur)
+	}
+}
+
+class QuarantineProducer {
+	KafkaProducer producer
+	def bootstrapServers = "127.0.0.1:9092"
+	def groupId = "my-fourth-application"
+
+	QuarantineProducer() {
+		def props = new Properties()
+		props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+		props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.getName())
+		props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.getName())
+		props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId)
+		props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
+		producer = new KafkaProducer<>(props)
+	}
+
+	Future<RecordMetadata> send(String key, String value) {
+		ProducerRecord quarantineRecord = new ProducerRecord("demo_java_quarantine", key, value)
+		producer.send(quarantineRecord, )
+		producer.flush()
+	}
+}
+
+class KafkaHandler {
+	Consumer consumer
+	QuarantineProducer quarantineProducer
+	def logger = LoggerFactory.getLogger(KafkaHandler)
+
+	KafkaHandler(Consumer consumer, QuarantineProducer quarantineProducer) {
+		this.consumer = consumer
+		this.quarantineProducer = quarantineProducer
+	}
+
 	void start(String topic, Closure callback) {
 		consumer.subscribe(List.of(topic))
 
@@ -42,7 +97,11 @@ class Consumer {
 		while (true) {
 			def records = consumer.poll(Duration.ofMillis(1000))
 			records.each {
-				callback(it)
+				try {
+					callback(it)
+				} catch (Exception e) {
+					quarantineProducer.send(it.key(), it.value())
+				}
 			}
 		}
 	}
@@ -50,7 +109,6 @@ class Consumer {
 
 class Payment {
 	String id
-	@JsonFormat (pattern = "yy-mm-dd HH:mm:ss")
 	Timestamp time
 	float amount
 }
